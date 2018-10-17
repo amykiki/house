8f77912f3c38e247a21100b4f6a46ba739a89a64
zoushumin3 <zoushumin@jd.com>
2018-10-17 17:10:45 +0800

    随机盐util

---
+++ house-common/src/main/java/com/april/house/common/util/secureSaltUtil.java
@@ -0,0 +1,9 @@
+package com.april.house.common.util;
+
+/**
+ * @Description : 产生安全的随机盐，使用SecureRandom
+ * @Creation Date:  2018-10-17 17:09
+ * --------  ---------  --------------------------
+ */
+public class secureSaltUtil {
+}
